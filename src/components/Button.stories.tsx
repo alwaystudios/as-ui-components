7e92152dc2d60550d5ae3d3a0e7020bef4026550
Gary Alway <gary.alway@acuris.com>
2020-10-11 10:58:42 +0100

    1.0.6

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -3,12 +3,50 @@ import { storiesOf } from '@storybook/react'
 import { Button } from './Button'
 import { boolean, number, text } from '@storybook/addon-knobs'
 import { AlarmIcon } from './icons/AlarmIcon'
+import { noop } from '../noop'
 
 const stories = storiesOf('Button', module)
 
 const icon = <AlarmIcon />
 
-stories.add('text only', () =>
+stories.add('text only', () => (
+  <Button
+    onClick={noop}
+    id={text('id', 'button-id')}
+    text={text('text', 'Save')}
+    className={text('className', '')}
+    isSubmit={boolean('isSubmit', false)}
+    disabled={boolean('disabled', false)}
+    tabIndex={number('tabIndex', 1)}
+  />
+))
+
+stories.add('text with icon', () => (
+  <Button
+    icon={icon}
+    onClick={noop}
+    id={text('id', 'button-id')}
+    text={text('text', 'Save')}
+    className={text('className', '')}
+    isSubmit={boolean('isSubmit', false)}
+    disabled={boolean('disabled', false)}
+    tabIndex={number('tabIndex', 1)}
+  />
+))
+
+stories.add('icon only', () => (
+  <Button
+    icon={icon}
+    onClick={noop}
+    id={text('id', 'button-id')}
+    className={text('className', '')}
+    isSubmit={boolean('isSubmit', false)}
+    disabled={boolean('disabled', false)}
+    tabIndex={number('tabIndex', 1)}
+  />
+))
+
+stories.add('with interactive state', () =>
   React.createElement(() => {
     const [counter, setCounter] = useState(0)
 
@@ -23,57 +61,6 @@ stories.add('text only', () =>
           onClick={onClick}
           id={text('id', 'button-id')}
           text={text('text', 'Save')}
-          className={text('className', '')}
-          isSubmit={boolean('isSubmit', false)}
-          disabled={boolean('disabled', false)}
-          tabIndex={number('tabIndex', 1)}
-        />
-      </>
-    )
-  }),
-)
-
-stories.add('text with icon', () =>
-  React.createElement(() => {
-    const [counter, setCounter] = useState(0)
-
-    const onClick = () => {
-      setCounter(counter + 1)
-    }
-
-    return (
-      <>
-        <p>Click counter: {counter}</p>
-        <Button
-          icon={icon}
-          onClick={onClick}
-          id={text('id', 'button-id')}
-          text={text('text', 'Save')}
-          className={text('className', '')}
-          isSubmit={boolean('isSubmit', false)}
-          disabled={boolean('disabled', false)}
-          tabIndex={number('tabIndex', 1)}
-        />
-      </>
-    )
-  }),
-)
-
-stories.add('icon only', () =>
-  React.createElement(() => {
-    const [counter, setCounter] = useState(0)
-
-    const onClick = () => {
-      setCounter(counter + 1)
-    }
-
-    return (
-      <>
-        <p>Click counter: {counter}</p>
-        <Button
-          icon={icon}
-          onClick={onClick}
-          id={text('id', 'button-id')}
           className={text('className', '')}
           isSubmit={boolean('isSubmit', false)}
           disabled={boolean('disabled', false)}

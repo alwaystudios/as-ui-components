a1fb7b4e9af12b13138f0068ba9a0b970aab1e0c
Gary Alway <garyalway@gmail.com>
2021-05-30 11:49:51 +0100

    bump libs

--- src/ImageCarousel.stories.tsx
+++ src/ImageCarousel.stories.tsx
@@ -7,24 +7,20 @@ const stories = storiesOf('Image carousel', module)
 const data = [
   {
     caption: 'Game casserole',
-    img:
-      'https://recipe-bible-content.s3-eu-west-1.amazonaws.com/recipes/game-casserole/Game%20casserole.jpg',
+    img: 'https://recipe-bible-content.s3-eu-west-1.amazonaws.com/recipes/game-casserole/Game%20casserole.jpg',
     onClick: () => alert('Game casserole'),
   },
   {
     caption: 'Lentil ragu parpadelle',
-    img:
-      'https://recipe-bible-content.s3-eu-west-1.amazonaws.com/recipes/lentil-ragu-parpadelle/Lentil%20ragu%20parpadelle.jpg',
+    img: 'https://recipe-bible-content.s3-eu-west-1.amazonaws.com/recipes/lentil-ragu-parpadelle/Lentil%20ragu%20parpadelle.jpg',
   },
   {
     caption: 'Loaded lentil salad with baked mango chutney feta',
-    img:
-      'https://recipe-bible-content.s3-eu-west-1.amazonaws.com/recipes/loaded-lentil-salad-with-baked-mango-chutney-feta/Lentil%20salad.JPG',
+    img: 'https://recipe-bible-content.s3-eu-west-1.amazonaws.com/recipes/loaded-lentil-salad-with-baked-mango-chutney-feta/Lentil%20salad.JPG',
   },
   {
     caption: 'Roasted butternut squash soup',
-    img:
-      'https://recipe-bible-content.s3-eu-west-1.amazonaws.com/recipes/roasted-butternut-squash-soup/butternut%20squash%20soup.jpg',
+    img: 'https://recipe-bible-content.s3-eu-west-1.amazonaws.com/recipes/roasted-butternut-squash-soup/butternut%20squash%20soup.jpg',
   },
 ]
 
